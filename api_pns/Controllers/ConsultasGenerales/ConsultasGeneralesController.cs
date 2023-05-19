ad3e6651d487af2d2e1c0b95a96e47a7410bbde5
María Paula Rondón <paularondon1016@gmail.com>
2023-05-19 13:09:37 -0500

    Add users module services
    Se realizan servicios para el modulo de usuarios

--- api_pns/Controllers/ConsultasGenerales/ConsultasGeneralesController.cs
+++ api_pns/Controllers/ConsultasGenerales/ConsultasGeneralesController.cs
@@ -1,5 +1,4 @@
 ﻿using api_pns.Context;
-using api_pns.Models.Tables;
 using api_pns.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -8,19 +7,19 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Threading.Tasks;
 using System;
-using api_pns.Models.Headquarters;
+using api_pns.Models.Consults;
 
-namespace api_pns.Controllers.Headquarters
+namespace api_pns.Controllers.ConsultasGenerales
 {
     [Route("api")]
     [ApiController]
-    public class SedesController : ControllerBase
+    public class ConsultasGeneralesController : ControllerBase
     {
         public Connection conn;
         private readonly IConfiguration _configuration;
         public ReplySucess oReply = new ReplySucess();
 
-        public SedesController(IConfiguration configuration)
+        public ConsultasGeneralesController(IConfiguration configuration)
         {
             _configuration = configuration;
             conn = new Connection();
@@ -36,7 +35,7 @@ namespace api_pns.Controllers.Headquarters
         /// </remarks>
         /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
         [HttpGet]
-        [Route("listHeadquarters")] 
+        [Route("listHeadquarters")]
         public async Task<IActionResult> listHeadquarters()
         {
             using (SqlConnection connection = conn.ConnectBD(_configuration))
@@ -55,11 +54,11 @@ namespace api_pns.Controllers.Headquarters
 
                     SqlDataReader sqldr = await cmd.ExecuteReaderAsync();
 
-                    List<HeadquartersModel> detail = new List<HeadquartersModel>();
+                    List<ConsultsModel> detail = new List<ConsultsModel>();
 
                     while (await sqldr.ReadAsync())
                     {
-                        HeadquartersModel detail2 = new HeadquartersModel();
+                        ConsultsModel detail2 = new ConsultsModel();
                         if (sqldr["id_headquarters"] != DBNull.Value) { detail2.idHeadquarters = Convert.ToInt32(sqldr["id_headquarters"]); } else { detail2.idHeadquarters = 0; }
                         if (sqldr["country_name"] != DBNull.Value) { detail2.countryName = sqldr["country_name"].ToString(); } else { detail2.countryName = ""; }
                         if (sqldr["city_name"] != DBNull.Value) { detail2.cityName = sqldr["city_name"].ToString(); } else { detail2.cityName = ""; }
@@ -158,6 +157,163 @@ namespace api_pns.Controllers.Headquarters
                     if (r.Flag == true)
                     {
                         r.Data = detail;
+                        r.Status = 200;
+
+                        oReply.Ok = true;
+                        oReply.Message = r.Message;
+                        oReply.Data = r.Data;
+
+                        return Ok(oReply);
+                    }
+                    else
+                    {
+                        oReply.Ok = false;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return BadRequest(oReply);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oReply.Ok = false;
+                    oReply.Message = ex.Message;
+                    oReply.Data = null;
+                    return BadRequest(oReply);
+                }
+            }
+        }
+        #endregion
+
+        #region Listar roles
+        // GET: api/listRoles
+        /// <summary>
+        /// Listar roles
+        /// </summary>
+        /// <remarks>
+        /// Método para listar roles
+        /// </remarks>
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+        [HttpGet]
+        [Route("listRoles")]
+        public async Task<IActionResult> listRoles()
+        {
+            using (SqlConnection connection = conn.ConnectBD(_configuration))
+            {
+                ReplyLogin r = new ReplyLogin();
+
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("sp_listRoles", connection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@message", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    SqlDataReader sqldr = await cmd.ExecuteReaderAsync();
+
+                    List<Dictionary<string, object>> details = new List<Dictionary<string, object>>();
+
+                    while (await sqldr.ReadAsync())
+                    {
+                        Dictionary<string, object> data = new Dictionary<string, object>();
+                        if (sqldr["id_role"] != DBNull.Value) { data.Add("idRole", Convert.ToInt32(sqldr["id_role"])); } else { data.Add("idRole", ""); }
+                        if (sqldr["name"] != DBNull.Value) { data.Add("name", sqldr["name"].ToString()); } else { data.Add("name", ""); }
+
+                        details.Add(data);
+                    }
+
+                    await sqldr.CloseAsync();
+
+                    r.Message = cmd.Parameters["@message"].Value != null ? cmd.Parameters["@message"].Value.ToString() : "";
+                    r.Flag = (bool)cmd.Parameters["@flag"].Value;
+                    r.Status = 400;
+
+
+                    if (r.Flag == true)
+                    {
+                        r.Data = details;
+                        r.Status = 200;
+
+                        oReply.Ok = true;
+                        oReply.Message = r.Message;
+                        oReply.Data = r.Data;
+
+                        return Ok(oReply);
+                    }
+                    else
+                    {
+                        oReply.Ok = false;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return BadRequest(oReply);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oReply.Ok = false;
+                    oReply.Message = ex.Message;
+                    oReply.Data = null;
+                    return BadRequest(oReply);
+                }
+            }
+        }
+        #endregion
+
+        #region Listar tipos de documento
+        // GET: api/listDocumentType
+        /// <summary>
+        /// Listar tipos de documento
+        /// </summary>
+        /// <remarks>
+        /// Método para listar tipos de documento
+        /// </remarks>
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+        [HttpGet]
+        [Route("listDocumentType")]
+        public async Task<IActionResult> listDocumentType()
+        {
+            using (SqlConnection connection = conn.ConnectBD(_configuration))
+            {
+                ReplyLogin r = new ReplyLogin();
+
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("sp_listDocumentType", connection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@message", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    SqlDataReader sqldr = await cmd.ExecuteReaderAsync();
+
+                    List<Dictionary<string, object>> details = new List<Dictionary<string, object>>();
+
+                    while (await sqldr.ReadAsync())
+                    {
+                        Dictionary<string, object> data = new Dictionary<string, object>();
+                        if (sqldr["id_document_type"] != DBNull.Value) { data.Add("idDocumentType", Convert.ToInt32(sqldr["id_document_type"])); } else { data.Add("idDocumentType", ""); }
+                        if (sqldr["prefix"] != DBNull.Value) { data.Add("prefix", sqldr["prefix"].ToString()); } else { data.Add("prefix", ""); }
+                        if (sqldr["name"] != DBNull.Value) { data.Add("name", sqldr["name"].ToString()); } else { data.Add("name", ""); }
+
+                        details.Add(data);
+                    }
+
+                    await sqldr.CloseAsync();
+
+                    r.Message = cmd.Parameters["@message"].Value != null ? cmd.Parameters["@message"].Value.ToString() : "";
+                    r.Flag = (bool)cmd.Parameters["@flag"].Value;
+                    r.Status = 400;
+
+
+                    if (r.Flag == true)
+                    {
+                        r.Data = details;
                         r.Status = 200;
 
                         oReply.Ok = true;
---
+++ api_pns/Controllers/Usuarios/UsuariosController.cs
@@ -0,0 +1,340 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using api_pns.Context;
+using api_pns.Models;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Threading.Tasks;
+using api_pns.Models.Users;
+
+namespace api_pns.Controllers.Usuarios
+{
+    [Route("api")]
+    [ApiController]
+    public class UsuariosController : Controller
+    {
+        public Connection conn; 
+        private readonly IConfiguration _configuration;
+        public ReplySucess oReply = new ReplySucess();
+
+        public UsuariosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            conn = new Connection();
+        }
+
+        #region Listar usuarios
+        // GET: api/listUsers
+        /// <summary>
+        /// Listar usuarios
+        /// </summary>
+        /// <remarks>
+        /// Método para consultar usuarios
+        /// </remarks>
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+        [HttpGet]
+        [Route("listUsers")]
+        public async Task<IActionResult> listUsers()
+        {
+            using (SqlConnection connection = conn.ConnectBD(_configuration))
+            {
+                ReplyLogin r = new ReplyLogin();
+
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("sp_listUsers", connection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@message", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    SqlDataReader sqldr = await cmd.ExecuteReaderAsync();
+
+                    List<Dictionary<string, object>> details = new List<Dictionary<string, object>>();
+
+                    while (await sqldr.ReadAsync())
+                    {
+                        Dictionary<string, object> data = new Dictionary<string, object>();
+                        if (sqldr["id_user"] != DBNull.Value) { data.Add("idUser", Convert.ToInt32(sqldr["id_user"])); } else { data.Add("idUser", ""); }
+                        if (sqldr["type_document"] != DBNull.Value) { data.Add("typeDocument", sqldr["type_document"].ToString()); } else { data.Add("typeDocument", ""); }
+                        if (sqldr["prefix"] != DBNull.Value) { data.Add("prefix", sqldr["prefix"].ToString()); } else { data.Add("prefix", ""); }
+                        if (sqldr["document_number"] != DBNull.Value) { data.Add("documentNumber", sqldr["document_number"].ToString()); } else { data.Add("documentNumber", ""); }
+                        if (sqldr["names"] != DBNull.Value) { data.Add("names", sqldr["names"].ToString()); } else { data.Add("names", ""); }
+                        if (sqldr["surnames"] != DBNull.Value) { data.Add("surnames", sqldr["surnames"].ToString()); } else { data.Add("surnames", ""); }
+                        if (sqldr["name_user"] != DBNull.Value) { data.Add("nameUser", sqldr["name_user"].ToString()); } else { data.Add("nameUser", ""); }
+                        if (sqldr["birth_date"] != DBNull.Value) { data.Add("birthDate", Convert.ToDateTime(sqldr["birth_date"])); } else { data.Add("birthDate", ""); }
+                        if (sqldr["name_conuntry"] != DBNull.Value) { data.Add("nameConuntry", sqldr["name_conuntry"].ToString()); } else { data.Add("nameConuntry", ""); }
+                        if (sqldr["name_city"] != DBNull.Value) { data.Add("nameCity", sqldr["name_city"].ToString()); } else { data.Add("nameCity", ""); }
+                        if (sqldr["name_role"] != DBNull.Value) { data.Add("nameRole", sqldr["name_role"].ToString()); } else { data.Add("nameRole", ""); }
+
+                        details.Add(data);
+                    }
+
+                    await sqldr.CloseAsync();
+
+                    r.Message = cmd.Parameters["@message"].Value != null ? cmd.Parameters["@message"].Value.ToString() : "";
+                    r.Flag = (bool)cmd.Parameters["@flag"].Value;
+                    r.Status = r.Flag ? 200 : 400;
+
+
+                    if (r.Flag)
+                    {
+                        r.Data = details;
+                        r.Message = "Successful countries";
+                        r.Status = 200;
+
+                        oReply.Ok = true;
+                        oReply.Message = r.Message;
+                        oReply.Data = r.Data;
+
+                        return Ok(oReply);
+                    }
+                    else
+                    {
+                        oReply.Ok = false;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return BadRequest(oReply);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oReply.Ok = false;
+                    oReply.Message = ex.Message;
+                    oReply.Data = null;
+                    return BadRequest(oReply);
+                }
+            }
+        }
+        #endregion
+
+        #region Detalle de usuarios
+        // GET: api/consultUser/{idUser}
+        /// <summary>
+        /// Detalle usuario
+        /// </summary>
+        /// <remarks>
+        /// Método para obtener detalle del usuario
+        /// </remarks>
+        /// <param name="idUser">Identificador del usuario</param>
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+        [HttpGet]
+        [Route("consultUser/{idUser}")]
+        public async Task<IActionResult> consultUser([FromRoute] int idUser)
+        {
+            using (SqlConnection connection = conn.ConnectBD(_configuration))
+            {
+                ReplyLogin r = new ReplyLogin();
+
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("sp_consultUser", connection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@id_user", idUser));
+                    cmd.Parameters.Add("@message", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    SqlDataReader sqldr = await cmd.ExecuteReaderAsync();
+
+                    Dictionary<string, object> data = new Dictionary<string, object>();
+
+                    while (await sqldr.ReadAsync())
+                    {
+                        if (sqldr["id_user"] != DBNull.Value) { data.Add("idUser", Convert.ToInt32(sqldr["id_user"])); } else { data.Add("idUser", ""); }
+                        if (sqldr["id_document_type"] != DBNull.Value) { data.Add("idDocumentType", Convert.ToInt32(sqldr["id_document_type"])); } else { data.Add("idDocumentType", ""); }
+                        if (sqldr["document_number"] != DBNull.Value) { data.Add("documentNumber", sqldr["document_number"].ToString()); } else { data.Add("documentNumber", ""); }
+                        if (sqldr["names"] != DBNull.Value) { data.Add("names", sqldr["names"].ToString()); } else { data.Add("names", ""); }
+                        if (sqldr["surnames"] != DBNull.Value) { data.Add("surnames", sqldr["surnames"].ToString()); } else { data.Add("surnames", ""); }
+                        if (sqldr["name_user"] != DBNull.Value) { data.Add("nameUser", sqldr["name_user"].ToString()); } else { data.Add("nameUser", ""); }
+                        if (sqldr["birth_date"] != DBNull.Value) { data.Add("birthDate", Convert.ToDateTime(sqldr["birth_date"])); } else { data.Add("birthDate", ""); }
+                        if (sqldr["id_country"] != DBNull.Value) { data.Add("idCountry", Convert.ToInt32(sqldr["id_country"])); } else { data.Add("idCountry", ""); }
+                        if (sqldr["id_city"] != DBNull.Value) { data.Add("idCity", Convert.ToInt32(sqldr["id_city"])); } else { data.Add("idCity", ""); }
+                        if (sqldr["id_role"] != DBNull.Value) { data.Add("idRole", Convert.ToInt32(sqldr["id_role"])); } else { data.Add("idRole", ""); }
+                    }
+
+                    await sqldr.CloseAsync();
+
+                    r.Message = cmd.Parameters["@message"].Value != null ? cmd.Parameters["@message"].Value.ToString() : "";
+                    r.Flag = (bool)cmd.Parameters["@flag"].Value;
+                    r.Status = r.Flag ? 200 : 400;
+
+
+                    if (r.Flag)
+                    {
+                        r.Data = data;
+
+                        oReply.Ok = true;
+                        oReply.Message = r.Message;
+                        oReply.Data = r.Data;
+
+                        return Ok(oReply);
+                    }
+                    else
+                    {
+                        oReply.Ok = false;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return BadRequest(oReply);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oReply.Ok = false;
+                    oReply.Message = ex.Message;
+                    oReply.Data = null;
+                    return BadRequest(oReply);
+                }
+            }
+        }
+        #endregion
+
+        #region Crear o actualizar usuario
+        // POST: api/createUpdateUsers
+        /// <summary>
+        /// Crear/Actualizar usuario
+        /// </summary>
+        /// <remarks>
+        /// Método para crear o actualizar usuario.
+        /// </remarks>
+        /// <param name="data">Data a validar en db</param>
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+        [HttpPost]
+        [Route("createUpdateUsers")]
+        public async Task<IActionResult> createUpdateCountry([FromBody] UsersModel data)
+        {
+            using (SqlConnection connection = conn.ConnectBD(_configuration))
+            {
+                ReplyLogin r = new ReplyLogin();
+
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("sp_createUpdateUsers", connection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@id_user", data.idUser));
+                    cmd.Parameters.Add(new SqlParameter("@document_number", data.documentNumber));
+                    cmd.Parameters.Add(new SqlParameter("@names", data.names));
+                    cmd.Parameters.Add(new SqlParameter("@surnames", data.surnames));
+                    cmd.Parameters.Add(new SqlParameter("@name_user", data.nameUser));
+                    cmd.Parameters.Add(new SqlParameter("@password", data.password));
+                    cmd.Parameters.Add(new SqlParameter("@birth_date", data.birthDate));
+                    cmd.Parameters.Add(new SqlParameter("@id_document_type", data.idDocumentType));
+                    cmd.Parameters.Add(new SqlParameter("@id_country", data.idCountry));
+                    cmd.Parameters.Add(new SqlParameter("@id_city", data.idCity));
+                    cmd.Parameters.Add(new SqlParameter("@id_role", data.idRole));
+
+                    cmd.Parameters.Add("@message", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    await cmd.ExecuteNonQueryAsync();
+
+                    r.Message = cmd.Parameters["@message"].Value != null ? cmd.Parameters["@message"].Value.ToString() : "";
+                    r.Flag = (bool)cmd.Parameters["@flag"].Value;
+                    r.Status = r.Flag == true ? 200 : 400;
+
+                    if (r.Flag)
+                    {
+                        oReply.Ok = r.Flag;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return Ok(oReply);
+                    }
+                    else
+                    {
+                        oReply.Ok = false;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return BadRequest(oReply);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oReply.Ok = false;
+                    oReply.Message = ex.Message;
+                    oReply.Data = null;
+                    return BadRequest(oReply);
+                }
+            }
+        }
+        #endregion
+
+        #region Actualizar contraseña
+        // POST: api/updatePassword
+        /// <summary>
+        /// Actualizar contraseña de usuario
+        /// </summary>
+        /// <remarks>
+        /// Método para actualizar contraseña de usuario.
+        /// </remarks>
+        /// <param name="data">Data a validar en db</param>
+        /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+        [HttpPost]
+        [Route("updatePassword")]
+        public async Task<IActionResult> updatePassword([FromBody] UserPasswordModel data)
+        {
+            using (SqlConnection connection = conn.ConnectBD(_configuration))
+            {
+                ReplyLogin r = new ReplyLogin();
+
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand cmd = new SqlCommand("sp_updatePassword", connection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@id_user", data.idUser));
+                    cmd.Parameters.Add(new SqlParameter("@password", data.password));
+
+                    cmd.Parameters.Add("@message", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    await cmd.ExecuteNonQueryAsync();
+
+                    r.Message = cmd.Parameters["@message"].Value != null ? cmd.Parameters["@message"].Value.ToString() : "";
+                    r.Flag = (bool)cmd.Parameters["@flag"].Value;
+                    r.Status = r.Flag == true ? 200 : 400;
+
+                    if (r.Flag)
+                    {
+                        oReply.Ok = r.Flag;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return Ok(oReply);
+                    }
+                    else
+                    {
+                        oReply.Ok = false;
+                        oReply.Message = r.Message;
+                        oReply.Data = null;
+
+                        return BadRequest(oReply);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oReply.Ok = false;
+                    oReply.Message = ex.Message;
+                    oReply.Data = null;
+                    return BadRequest(oReply);
+                }
+            }
+        }
+        #endregion
+    }
+}
